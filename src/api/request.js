fb39454b2b57de33229f0ee559c18d1318c14b4c
unknown <bogdan.lyamzin.d@gmail.com>
2023-08-16 14:55:51 +0300

    add revalidate example

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,12 @@
 const {NEXT_PUBLIC_API_URL} = process.env;
 
 const request = async (url, options = {}) => {
-    const response = await fetch(`https://62becfba0bc9b125615fd0f7.mockapi.io/api/${url}`, {...options});
+    const response = await fetch(`https://62becfba0bc9b125615fd0f7.mockapi.io/api/${url}`, {
+        ...options,
+        next: {
+            revalidate: 30,
+        }
+    });
 
     const result = await response.json();
 
